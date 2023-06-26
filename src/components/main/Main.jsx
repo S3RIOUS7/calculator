9bb25c9c7f78fdc8eb5bb095e935a7ade5e8da4c
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-06-26 13:12:24 +0300

    last commit

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,7 +4,7 @@ import "./main.scss"
 import Input from "../input/Input";
 
 
-const numbersSign = [{id:0, text:0}, {id:1, text:1}, {id:2, text:2}, {id:3, text:3}, {id:4, text:4}, {id:5, text:5}, {id:6, text:6}, {id:7, text:7}, {id:8, text:8}, {id:9, text:9}, , {id:10, text:"."}, {id:11, text:"+"}, {id:12, text:"-"}, {id:13, text:"*"}, {id:14, text:"/"}, {id:15, text:"="}, {id:16, text:"C"}]
+const numbersSign = [{id:0, text:0}, {id:1, text:1}, {id:2, text:2}, {id:3, text:3}, {id:4, text:4}, {id:5, text:5}, {id:6, text:6}, {id:7, text:7}, {id:8, text:8}, {id:9, text:9}, , {id:10, text:"."}, {id:11, text:"+"}, {id:12, text:"-"}, {id:13, text:"*"}, {id:14, text:"/"}, {id:15, text:"C"}, {id:16, text:"="}]
 
 function Main (){
     const [displayText, setDisplayText] = useState("");
@@ -14,26 +14,35 @@ function Main (){
 
 
     const getValueInput = (event) => {
-      setDisplayText(event.target.value);}
+     console.log(event.target.value)
+      setDisplayText(event.target.value)
+    //  if(displayText[0] === '0'){
+    //     if(displayText.includes(".")){
+    //      setDisplayText( event.target.value);
+    //     }
+    //     } else{
+    //       setDisplayText(event.target.value);
+    //     }
+    }
 
     const ouputNumbers = (text, id) => {
 
+     
+      console.log(typeof displayText)
+    
 
+      if(id >= 0 && id <= 9){
         if(displayText[0] === '0'){
-            setDisplayText('')
-
-        }
-
-        if(id >= 0 && id <= 9){
-        setDisplayText(prevText => prevText + text);
-      }
-
-        else if(id === 16){
+           if(displayText.includes(".")){
+            setDisplayText(prevText => prevText + text);
+           }
+           } else{
+             setDisplayText(prevText => prevText + text);
+           }
+      } else if (id === 15){
             setDisplayText('')
             setSaveText('')
-        }
-       
-        else if (id >= 11 && id <= 14) {
+        }else if (id >= 11 && id <= 14) {
             setSaveText(displayText);
             setDisplayText("");
             const operations = {
@@ -45,15 +54,11 @@ function Main (){
             if (id in operations) {
               setCalculationResult(operations[id]);
             }
-
-          }
-          else if (id === 10 ) {
+          } else if (id === 10 ) {
             if (!displayText.includes(".")) {
               setDisplayText(prevText => prevText + text);
             }
-          }
-        
-        else if(id === 15){
+          }else if(id === 16){
             if (saveText !== "" && displayText !== "") {
                 const operations = {
                   "+": () => "" + (+saveText + +displayText),
@@ -69,20 +74,15 @@ function Main (){
               }
         }   
     };
- 
-    
+
 //asd
     return(
     <div className="container">
        <div className="inputMax"><Input type='text' value={displayText} onChange={(event) => getValueInput(event)} /></div>
       <div className="allButtons">
-        {numbersSign.map(content => (<div key = {content.id} ><button className={content.id === 16 ? "btnClear" : content.id <= 10 ? "btn" : "btncount" } onClick={() => ouputNumbers(content.text, content.id)}>{content.text}</button></div>))}
+        {numbersSign.map(content => (<div key = {content.id} ><button className={content.id === 16  ? "btnClear" : content.id <= 10 ? "btn" : "btncount" } onClick={() => ouputNumbers(content.text, content.id)}>{content.text}</button></div>))}
     </div>
-      
-    
 
-    
-    
     </div>
 )
 }
